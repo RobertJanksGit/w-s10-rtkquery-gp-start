05b6d716cd6a8e22f61d01d62d9cd4d67bdf2f35
Robert Jank <robert.jank@yahoo.com>
2024-09-02 17:31:45 -0500

    finished

--- frontend/components/Todos.js
+++ frontend/components/Todos.js
@@ -1,42 +1,63 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useSelector, useDispatch } from 'react-redux'
-import { toggleShowCompletedTodos } from '../state/todosSlice'
+import React from "react";
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleShowCompletedTodos } from "../state/todosSlice";
+import { useGetTodosQuery, useToggleTodoMutation } from "../state/todosApi";
 
 const StyledTodo = styled.li`
-  text-decoration: ${pr => pr.$complete ? 'line-through' : 'initial'};
+  text-decoration: ${(pr) => (pr.$complete ? "line-through" : "initial")};
   cursor: pointer;
-`
+`;
 
 export default function Todo() {
+  // rtk query
+  const {
+    data: todos,
+    isLoading: todosLoading,
+    isFetching: todosRefreshing,
+  } = useGetTodosQuery();
+  const [toggleTodo, { error: toggleError, isLoading: todosToggling }] =
+    useToggleTodoMutation();
   // redux
-  const showCompletedTodos = useSelector(st => st.todosState.showCompletedTodos)
-  const dispatch = useDispatch()
+  const showCompletedTodos = useSelector(
+    (st) => st.todosState.showCompletedTodos
+  );
+  const dispatch = useDispatch();
   return (
     <div id="todos">
-      <div className="error"></div>
-      <h3>Todos</h3>
+      <div className="error">{toggleError && toggleError.data.message}</div>
+      <h3>Todos {todosToggling || (todosRefreshing && "being updated")}</h3>
       <ul>
-        {
-          [
-            { id: 1, label: 'Laundry', complete: true },
-            { id: 2, label: 'Groceries', complete: false },
-            { id: 3, label: 'Dishes', complete: false },
-          ].filter(todo => {
-            return showCompletedTodos || !todo.complete
-          })
-            .map(todo => {
-              return (
-                <StyledTodo $complete={todo.complete} key={todo.id}>
-                  <span>{todo.label}{todo.complete && ' ✔️'}</span>
-                </StyledTodo>
-              )
-            })
-        }
+        {todosLoading
+          ? "todos loading..."
+          : todos
+              ?.filter((todo) => {
+                return showCompletedTodos || !todo.complete;
+              })
+              .map((todo) => {
+                const onToggle = () => {
+                  toggleTodo({
+                    id: todo.id,
+                    todo: { complete: !todo.complete },
+                  });
+                };
+                return (
+                  <StyledTodo
+                    onClick={onToggle}
+                    $complete={todo.complete}
+                    key={todo.id}
+                  >
+                    <span>
+                      {todo.label}
+                      {todo.complete && " ✔️"}
+                    </span>
+                  </StyledTodo>
+                );
+              })}
       </ul>
       <button onClick={() => dispatch(toggleShowCompletedTodos())}>
-        {showCompletedTodos ? 'Hide' : 'Show'} completed todos
+        {showCompletedTodos ? "Hide" : "Show"} completed todos
       </button>
     </div>
-  )
+  );
 }
--- frontend/state/store.js
+++ frontend/state/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
-import todosReducer from './todosSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import todosReducer from "./todosSlice";
+import { todosApi } from "./todosApi";
 
 export const store = configureStore({
   reducer: {
     todosState: todosReducer,
+    [todosApi.reducerPath]: todosApi.reducer,
   },
-})
+  middleware: (getDefault) => getDefault().concat(todosApi.middleware),
+});
